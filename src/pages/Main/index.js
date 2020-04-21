0b9bc60b50c42c5ca50cc8e8028ad24656402f30
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-04-21 19:26:20 -0300

    Update files

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,11 +7,12 @@ import './index.css';
 
 export default function MainPage(){
     const [projetos, setProjetos] = useState([]);
+    console.log(process.env.GITHUB_TOKEN)
     useEffect(() => {
         unirest
-        .get(`https://api.github.com/user/repos?visibility=public&affiliation=owner`)
+        .get(`https://api.github.com/user/repos?visibility=public`)
         .headers({
-            "authorization": `token ae67855cdb4b88783423ad3c4366fe88721aea86`,
+            "authorization": 'token d927b1de9749775137a1efe41e76ac93c09afcb1',
             "user-agent":`rodcordeiro`
           })
         .then((response) => {
@@ -20,23 +21,56 @@ export default function MainPage(){
         })
     },[]);
     
+    function formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+    
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+    
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
 
-    return (
+    function handleProject(e,project){
+        e.preventDefault();
+        localStorage.setItem('project', project)
+    }
+        return (
         <div className='container'>
             
             <div className='content'>
-                <h1>Repositórios</h1>
+                <header>
+                    <img src="https://rodcordeiro.github.io/shares/img/RC-W.png" alt="logo" />
+                    <h1>Repositórios</h1>
+                </header>
                 <hr />
-                <ul className='repositories'>
-                    {projetos.map(projeto=>(
-                        <li className='repository' key={projeto.id}>
-                            <h3>{projeto.name}</h3>
-                            <p>{projeto.description || "Something that just god, and maybe me, knows what it does"}</p>
-                            <Link to='/'>Ver detalhes</Link>
-                        </li>
-                    )
-                    )}
-                </ul>            
+                <table>
+                <tr className='line'>
+                        <th>Repository</th>
+                        <th>Description</th>
+                        <th>Created</th>
+                        <th>Language</th>
+                        <th>Size</th>
+                    </tr>
+                    {projetos.map(projeto=>{
+                        if(projeto.fork !== true){
+                            return (
+                                <tr className='line' key={projeto.id}>
+                                <Link to='/' onClick={(e)=>handleProject(e,projeto)}>
+                                    <td>{projeto.name}</td>
+                                </Link>
+                                <td>{projeto.description || (<i>"Something that I need to read again to remember what it does."</i>)}</td>
+                                <td>{projeto.created_at}</td>
+                                <td>{projeto.language || "¯\_(ツ)_/¯"}</td>
+                                <td>{formatBytes(projeto.size)}</td>
+                            </tr>
+                            )
+                        }
+                    })}
+                </table>
+                
             </div>
             
         </div>
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MainPage from './pages/Main';
+import ProjectPage from './pages/Project';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={MainPage} />
+                <Route path="/" component={ProjectPage} />
             </Switch>
         </BrowserRouter>
     );
